3279861a2dfc31c47a64ed0138b91150c202feb9
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2024-04-24 00:36:05 -0400

    indents source file with two spaces

--- src/com/midiaz/menu/Menu.java
+++ src/com/midiaz/menu/Menu.java
@@ -4,48 +4,48 @@ import java.util.Scanner;
 
 public class Menu
 {
-    private int tag = 0;
-    private Scanner scan = null;
+  private int tag = 0;
+  private Scanner scan = null;
 
-    public Menu (Scanner scan)
-    {
-	this.scan = scan;
+  public Menu (Scanner scan)
+  {
+    this.scan = scan;
+  }
+
+  public int tag ()
+  {
+    return this.tag;
+  }
+
+  public void prompt ()
+  {
+    String request = "Please enter the tag of the course you wish to grade ";
+    String legend = "(0: Math 1: Spanish 2: English):";
+    String msg = (request + legend);
+    System.out.printf(msg);
+
+    this.tag = scan.nextInt();
+    if (this.tag < 0 || this.tag > 2) {
+      System.out.println("Please input a valid tag");
+      this.prompt();
     }
 
-    public int tag ()
-    {
-	return this.tag;
+    switch (this.tag) {
+      case 0:
+	System.out.println("Math");
+	break;
+      case 1:
+	System.out.println("Spanish");
+	break;
+      default:
+	System.out.println("English");
     }
-
-    public void prompt ()
-    {
-	String request = "Please enter the tag of the course you wish to grade ";
-	String legend = "(0: Math 1: Spanish 2: English):";
-	String msg = (request + legend);
-	System.out.printf(msg);
-
-	this.tag = scan.nextInt();
-	if (this.tag < 0 || this.tag > 2) {
-	    System.out.println("Please input a valid tag");
-	    this.prompt();
-	}
-
-	switch (this.tag) {
-	    case 0:
-		System.out.println("Math");
-		break;
-	    case 1:
-		System.out.println("Spanish");
-		break;
-	    default:
-		System.out.println("English");
-	}
-    }
+  }
 }
 
 /*
 
-JGradeApp					April 23, 2024
+   JGradeApp					April 23, 2024
 
 source: Menu.java
 author: misael-diaz
